efa09f09fcdd58375402ce9644213173fe069371
Tim Tsu <timtsu@gmail.com>
2012-07-10 01:51:18 -0500

    replace tabs. auto launch vim BundleInstall

--- homemake.sh
+++ homemake.sh
@@ -8,40 +8,41 @@ FILES="vimrc zshrc"
 
 # Create backup dir
 if [ ! -d $BACKUP_DIR ]; then
-	echo "Creating $BACKUP_DIR for dotfile backup"
-	mkdir -p $BACKUP_DIR
+  echo "Creating $BACKUP_DIR for dotfile backup"
+  mkdir -p $BACKUP_DIR
 fi
 
 for FILE in $FILES; do
-	if [ -e $DOTFILES_DIR/$FILE ]; then
+  if [ -e $DOTFILES_DIR/$FILE ]; then
 
-		if [ -h ~/.$FILE ]; then
-			echo "~/.$FILE is already linked"
-			continue
-		elif [ -e ~/.$FILE ]; then
-			echo "Found existing .$FILE. Moving to $BACKUP_DIR" 
-			mv -i ~/.$FILE $BACKUP_DIR/
-		else
-			echo "linking .$FILE"
-			ln -s $DOTFILES_DIR/$FILE ~/.$FILE
-		fi
-	fi
+    if [ -h ~/.$FILE ]; then
+      echo "~/.$FILE is already linked"
+      continue
+    elif [ -e ~/.$FILE ]; then
+      echo "Found existing .$FILE. Moving to $BACKUP_DIR" 
+      mv -i ~/.$FILE $BACKUP_DIR/
+    else
+      echo "linking .$FILE"
+      ln -s $DOTFILES_DIR/$FILE ~/.$FILE
+    fi
+  fi
 done
 
 # Install oh-my-zsh
 if [ -d ~/.oh-my-zsh ]; then
-	echo "You already have Oh My Zsh installed"
+  echo "You already have Oh My Zsh installed"
 else
-	echo "Auto-installing Oh My Zsh..."
-	curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  echo "Auto-installing Oh My Zsh..."
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 fi
 
 # Install vundle
 if [ -d ~/.vim/bundle/vundle ]; then
-	echo "You already have vundle installed"
+  echo "You already have vundle installed"
 else
-	echo "Installing vundle..."
- 	git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  echo "Installing vundle..."
+  git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  vim +BundleInstall +qall
 fi
 
 echo "Furnished!"
