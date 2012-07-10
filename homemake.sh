cdcdc64984deb74857db8fa25d63c4b18f9d19f2
Tim Tsu <timtsu@gmail.com>
2012-07-10 01:23:51 -0500

    add zshrc. install oh-my-zsh

--- homemake.sh
+++ homemake.sh
@@ -4,7 +4,7 @@
 
 DOTFILES_DIR=.
 BACKUP_DIR=~/.dotfiles.old
-FILES="vimrc zshrc oh-my-zsh"
+FILES="vimrc zshrc"
 
 # Create backup dir
 if [ ! -d $BACKUP_DIR ]; then
@@ -15,21 +15,33 @@ fi
 for FILE in $FILES; do
 	if [ -e $DOTFILES_DIR/$FILE ]; then
 
-		if [ -e ~/.$FILE && ! -h ~/.$FILE ]; then
+		if [ -h ~/.$FILE ]; then
+			echo "~/.$FILE is already linked"
+			continue
+		elif [ -e ~/.$FILE ]; then
 			echo "Found existing .$FILE. Moving to $BACKUP_DIR" 
 			mv -i ~/.$FILE $BACKUP_DIR/
+		else
+			echo "linking .$FILE"
+			ln -s $DOTFILES_DIR/$FILE ~/.$FILE
 		fi
-
-		echo "linking .$FILE"
-		ln -s $DOTFILES_DIR/$FILE ~/.$FILE
 	fi
 done
 
-if [ -d ~/.vim/bundle/vundle ]; then
-  echo "You already have vundle installed"
+# Install oh-my-zsh
+if [ -d ~/.oh-my-zsh ]; then
+	echo "You already have Oh My Zsh installed"
 else
-  echo "Cloning vundle"
-  git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+	echo "Auto-installing Oh My Zsh..."
+	curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+fi
+
+# Install vundle
+if [ -d ~/.vim/bundle/vundle ]; then
+	echo "You already have vundle installed"
+else
+	echo "Installing vundle..."
+ 	git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
 
 echo "Furnished!"
